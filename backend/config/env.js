7419ce5dab0eca07767af99e0aa64bf46e380951
it22242204 <128572527+it22242204@users.noreply.github.com>
2025-03-26 15:59:47 +0530

    auth updated

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,6 +4,7 @@ dotenv.config();
 const PORT = process.env.PORT || 2025;
 const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
 const MONGO_URI = process.env.MONGO_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
-module.exports = { PORT, BASE_URL, MONGO_URI };
+module.exports = { PORT, BASE_URL, MONGO_URI, JWT_SECRET };
--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['Admin', 'RestaurantManager', 'User'], required: true }
+    role: { type: String, enum: ['Admin', 'RestaurantManager', 'User','deliverer'], required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
